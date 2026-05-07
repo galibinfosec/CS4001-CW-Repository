338556ef5f6f6945b1a4771bb8766947955f3993
galibinfosec <lm2590013@cityu.ac.cy>
2026-05-07 22:42:28 +0300

    Update Mobile.java

--- Mobile.java
+++ Mobile.java
@@ -3,7 +3,7 @@ public class Mobile extends Gadget {
 
     private int callingCredit;
 
-    
+
     public Mobile(String model, double price, int weight, String size, int callingCredit) {
         super(model, price, weight, size);
         this.callingCredit = callingCredit;
@@ -14,7 +14,7 @@ public class Mobile extends Gadget {
         return callingCredit;
     }
 
-    
+ 
     public void addCredit(int amount) {
         if (amount > 0) {
             callingCredit += amount;
@@ -24,7 +24,7 @@ public class Mobile extends Gadget {
         }
     }
 
-    
+
     public void makeCall(String phoneNumber, int duration) {
         if (callingCredit >= duration) {
             System.out.println("Calling " + phoneNumber + " for " + duration + " minute(s).");
@@ -35,8 +35,7 @@ public class Mobile extends Gadget {
         }
     }
 
-    
-    @Override
+   
     public void display() {
         super.display();
         System.out.println("Calling Credit: " + callingCredit + " minutes");
